b75ce533fdfa9730fdea719af950f2574fb2091b
Pix17 <giuseppecapicotto7@gmail.com>
2021-08-10 17:05:55 +0200

    update: lista parole

--- script/script_impiccato.js
+++ script/script_impiccato.js
@@ -1,4 +1,4 @@
-// Array standard da 15 lettere
+// Array base da 15 lettere
 var lettere = [
  lettera1 = document.getElementById("letter1"),
  lettera2 = document.getElementById("letter2"),
@@ -18,7 +18,39 @@ var lettere = [
 ];
 
 // Array delle parole possibili
-const parola = ["casa","canino","criceto","mobilio","arcivescovo","pokemon","troll","apple","windows","gatto","cane","ragazza"];
+const parola = [
+    "ala","albero","acqua","aceto","abisso","acaro","alligatore","anatra","antilope","aragosta","avvoltoio","amsterdam","abbaco","accappatoio","accendino","aereoplano","almanacco","ambulanza","aquilone",
+    "bar","baco","babbuino","balena","barracuda","bradipo","berlino","baionetta","bambola","bambolotto","bandiera","baule","banana","bicicletta","boomerang",
+    "cane","camaleonte","cammello","canarino","canguro","capodoglio","cavallo","cerbiatto","chiocciola","coccinella","coccodrillo","coniglio","copenaghen","calcolatrice","calendario","calzamaglia","camomilla",
+        "cannocchiale","cappello","cassaforte","castello","clavicembalo","comodino","colla","computer","congelatore","cristallo","cucchiaio","chitarra",
+    "dado","dama","dito","daino","delfino","dinosauro","dobermann","drago","damasco","dublino","dardo","denaro","dentifricio","deodorante","diaframma","distintivo",
+    "elefante","ermellino","edimburgo","elicottero","equalizzatore","evidenziatore","etichettatrice",
+    "foglia","facocero","falco","fenicottero","formica","furetto","fantoccio","fagotto","farmacia","fiaccola","fiammifero","filigrana","finestra","fisarmonica","fioretto","fischietto","fuoco","forchetta","formaggio",
+        "fotocamera","fotocopiatrice","francobollo","frusta",
+    "gioco","gabbiano","gallina","gamberone","gazzella","ghepardo","granchio","gufo","gorilla","gavettone","gemelli","gigante","giocattolo","gioiello","giroscopio","gondola","grammofono","grappa",
+    "hotel",
+    "istrice","iena","iguana","ippopotamo","insetto","idrovolante","imbuto","impermeabile","inchiostro","ingranaggio","insalatiera",
+    "juventus",
+    "koala",
+    "lana","labrador","lamantino","lucertola","lussemburgo","lacrimogeno","lampadario","lampadina","lanciafiamme","lavatrice","legna","locomotiva","lubrificante",
+    "mare","maggiolino","macaco","maremmano","martello","merluzzo","montecarlo","macchina","medaglia","maglia","magnete","mangiacassette","mappamondo","matassa","materasso","mattarello","metropolitana","microprocessore","mongolfiera","mulinello",
+    "naso","nastro","nave","navigatore","navicella","negozio","nebulizzatore","neon","noccioline",
+    "orca","ornitorinco","orso","obliteratrice","occhiali","ombrello","ombrellone","ossigeno","osso",
+    "pinguino","panda","pappagallo","pastore","pellicano","pipistrello","porcospino","panama","pechino","parigi","pianoforte","pagliaccio","paladino","palla","pallone","pallottola","pannolino","pantaloni","pennello","paracadute","parafulmine",
+        "passaporto","passamontagna","patata","penna","piccone","pneumatico","portafogli","puzzle",
+    "quad","quercia","quaderno","quadro",
+    "rana","rinoceronte","roditore","roma","racchetta","radio","rastrello","riflettore","rimorchio","rivoltella","rompighiaccio","rossetto",
+    "serpente","salamandra","scarabeo","scimmia","squalo","sirena","singapore","stoccolma","sirena","sarcofago","sapone","satellite","scala","scanner","sciarpa","scorpione","schiaccianoci","scolapasta","scooter","scudetto","semaforo","sfigmomanometro",
+        "sigarette","smeraldo","soprammobile","spaventapasseri","spazzacamino",
+    "tonno","torta","tacchino","tarantola","tartaruga","talismano","tamburo","tappeto","tassametro","tastiera","telegramma","telecamera","telecomando","telefono","televisore","temperamatite","termometro","termosifone","torpediniera","tosaerba","trampolino","transatlantico",
+    "uva","uccello","uragano","umidificatore","utilitaria",
+    "vagone","varano","vespa","vitello","volpe","vienna","vapore","vascello","valanga","vespa","vetro","videocamera","violoncello","vocabolario",
+    "wireless",
+    "xilofono",
+    "yoga",
+    "zanzara","zaino","zaffiro","zebra","zerbino","zucchero"
+    
+];
 
 // Array delle immagini
 var immagini = [
@@ -54,7 +86,7 @@ var fine = false;
 // Funzione principale
 try_btn.onclick = function(){
     if(!fine){
-        var tentativo = input.value;
+        var tentativo = input.value.toLowerCase();
         
         for(let i=0; i<parola_length; i++){
             if(parola_scelta[i] == tentativo){
